6da16f8d693f5af1afb4d61a5a4ccf4d46a5061f
yuki66 <nktiga6@gmail.com>
2015-01-30 22:27:18 +0900

    email miss

--- test/ViewController.m
+++ test/ViewController.m
@@ -19,8 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-
-
+//a//
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
